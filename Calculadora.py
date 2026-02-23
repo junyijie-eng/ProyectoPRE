b31040747ba059d2b00ec758911aeffdccdce555
Junyi Jie <jieyjunyi@gmail.com>
2026-02-23 18:47:58 +0100

    div

--- Calculadora.py
+++ Calculadora.py
@@ -4,3 +4,8 @@ def resta(a,b):
     return a - b
 def multiplicacion(a,b):
     return a * b
+def division(a,b):
+    if b == 0:
+        return "Error: No se puede dividir por cero"
+    else:
+        return a / b
